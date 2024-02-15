e3c6ad312f059be79f7020f25218ec5fa8eea6d2
AlessandroMason <ale.mason03@gmail.com>
2024-02-15 09:09:22 -0400

    brugnera update

--- gsap_animation_script.js
+++ gsap_animation_script.js
@@ -34,3 +34,89 @@ mm.add("(min-width: 900px)", () => {
         })
     })
 });
+
+let tl_cover = gsap.timeline();
+
+tl_cover.from(".cover__content__text h1", {opacity: 0, y: 150, duration: 1, ease: "power1.inOut"})
+.from(".cover__content__text p", {opacity: 0, y: 150, duration: 0.7, ease: "power1.inOut"})
+        .from(".cover__img", {opacity: 0, x: 250, duration: 0.7, ease: "power1.inOut"})
+        .from(".cover__btn a", {opacity: 0, y: 150, duration: 1, stagger: 0.3, ease: "power1.inOut"}, 1.7);
+
+gsap.from(".chart h2", {
+    opacity: 0,
+    y: 50,
+
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".chart",
+        start: "top 70%",
+        end: "top 30%",
+        scrub: 1,
+        toggleActions: "play reverse play reverse",
+    }
+})
+
+gsap.from(".chart__img", {
+    opacity: 0,
+    scale: 0.7,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".chart__img",
+        start: "top bottom",
+        end: "top 10%",
+        toggleActions: "play reverse play reverse",
+        scrub: true,
+    }
+})
+
+gsap.from(".features__title", {
+    opacity: 0,
+    y: 50,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".features",
+        start: "top 70%",
+        end: "top 30%",
+        scrub: 1,
+        toggleActions: "play reverse play reverse",
+    }
+})
+
+gsap.from(".outro__title", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".outro",
+        start: "top 70%",
+        toggleActions: "play reverse play reverse",
+    }
+})
+
+gsap.from(".outro__btn a", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    stagger: 0.3,
+    delay: 0.3,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".outro",
+        start: "top 70%",
+        toggleActions: "play reverse play reverse",
+    }
+})
+
+gsap.from(".outro__img", {
+    opacity: 0,
+    y: 150,
+    duration: 1,
+    delay: 0.5,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".outro",
+        start: "top 70%",
+        toggleActions: "play reverse play reverse",
+    }
+})
